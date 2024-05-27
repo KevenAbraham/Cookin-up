d320b0db2801e7d2620e91d6cb5f293963dd8a44
KevenAbraham <abraham.keven20@gmail.com>
2024-05-27 20:03:38 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import Banner from './components/Banner.vue';
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
+
+export default {
+  components: { Banner, ConteudoPrincipal }
+}
+</script>
+
+<template>
+  <Banner />
+  <ConteudoPrincipal />
+</template>
+
+<style scoped> /*Qualquer estilo que for colocado aqui, afetará somente a esse escopo*/
+</style>
